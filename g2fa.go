c664530d83a2f43028824fc0deba92154d3b2ffe
Andrew Romanenco <andrew.romanenco@shopify.com>
2016-08-18 16:24:20 -0400

    Add code extraction from mac RFC4226

--- g2fa.go
+++ g2fa.go
@@ -30,3 +30,17 @@ func EncodeKey(key []byte) string {
 func DecodeKey(skey string) ([]byte, error) {
 	return base32.StdEncoding.DecodeString(strings.ToUpper(skey))
 }
+
+// decodeHMAC extracts code from a HMAC according to RFC4226
+func decodeHMAC(hash []byte) int32 {
+	if len(hash) != 20 {
+		panic("Not a HMAC-SHA1 value")
+	}
+	offset := hash[19] & 0xf
+	binCode := int32(0)
+	binCode += int32(hash[offset+3])
+	binCode += (int32(hash[offset+2]) << 8)
+	binCode += (int32(hash[offset+1]) << 16)
+	binCode += (int32(hash[offset]&0x7f) << 24)
+	return binCode % 1000000
+}
--- g2fa_test.go
+++ g2fa_test.go
@@ -52,3 +52,12 @@ func TestDecodeKeyFailsIfInputIsNotCorrect(t *testing.T) {
 		t.Error("Must fail if input is not write encoding")
 	}
 }
+
+// testDecodeHMAC uses test data from RFC4226/5.4
+func testDecodeHMAC(t *testing.T) {
+	hmac := []byte{0x1f, 0x86, 0x98, 0x69, 0x0e, 0x02, 0xca, 0x16, 0x61, 0x85, 0x50, 0xef, 0x7f, 0x19, 0xda, 0x8e, 0x94, 0x5b, 0x55, 0x5a}
+	code := decodeHMAC(hmac)
+	if code != 872921 {
+		t.Error("Returned code does not match expectation")
+	}
+}
